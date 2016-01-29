e7d9d0c0e6cf1b5e3544aacccfdf07d505698b5f
Giovanni Di Milia <gdimilia@mit.edu>
2016-01-29 11:33:08 -0500

    Work in progress

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
-	"strings"
 	"os"
+	"strings"
+)
+import (
+	"go_review_counter/utils"
 )
 
 const APP_VERSION = "0.1"
@@ -13,23 +16,26 @@ var (
 	versionFlag *bool   = flag.Bool("v", false, "Print the version number.")
 	sentinel    *string = flag.String("sentinel", ":1:", "String to look for to count something as \"reviewed\"")
 	repos       *string = flag.String("repos", "", "Repos to search through")
+	token       *string = flag.String("token", "", "Your github personal access token")
 )
+var repoList []string
 
 func parseArgs() {
 	flag.Parse()
 
+	// version
 	if *versionFlag {
 		fmt.Println("Version:", APP_VERSION)
 		os.Exit(0)
 	}
-	if *repos == "" {
+	// repos to be checked
+	if utils.IsEmpty(*repos) {
 		flag.PrintDefaults()
 	}
-	repoList := strings.Split(*repos, " ")
-
-	return repoList
+	repoList = strings.Split(*repos, " ")
 }
 
 func main() {
+	// parse the input arguments
 	parseArgs()
 }
---
+++ utils/is_empty.go
@@ -0,0 +1,9 @@
+package utils
+
+import (
+	"strings"
+)
+
+func IsEmpty(str string) bool {
+	return strings.TrimSpace(str) == ""
+}
