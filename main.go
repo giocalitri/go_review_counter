f86094de91c385bb3b6090c7cf23cb0227e13226
Giovanni Di Milia <gdimilia@mit.edu>
2016-01-29 11:33:08 -0500

    Work in progress

--- main.go
+++ main.go
@@ -1,20 +1,35 @@
 package main
 
 import (
-    "flag"
-    "fmt"
+	"flag"
+	"fmt"
+	"strings"
+	"os"
 )
 
 const APP_VERSION = "0.1"
 
-// The flag package provides a default help printer via -h switch
-var versionFlag *bool = flag.Bool("v", false, "Print the version number.")
+var (
+	versionFlag *bool   = flag.Bool("v", false, "Print the version number.")
+	sentinel    *string = flag.String("sentinel", ":1:", "String to look for to count something as \"reviewed\"")
+	repos       *string = flag.String("repos", "", "Repos to search through")
+)
 
-func main() {
-    flag.Parse() // Scan the arguments list
+func parseArgs() {
+	flag.Parse()
 
-    if *versionFlag {
-        fmt.Println("Version:", APP_VERSION)
-    }
+	if *versionFlag {
+		fmt.Println("Version:", APP_VERSION)
+		os.Exit(0)
+	}
+	if *repos == "" {
+		flag.PrintDefaults()
+	}
+	repoList := strings.Split(*repos, " ")
+
+	return repoList
 }
 
+func main() {
+	parseArgs()
+}
